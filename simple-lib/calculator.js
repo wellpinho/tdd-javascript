b7b183a8818c084b626cab6dbb1a9ea1ab6f5d36
wellington.pinho@ahgora.com.br <wellington.pinho@ahgora.com.br>
2022-08-23 20:19:27 -0300

    ✔️ PASS: error if params is not number

--- simple-lib/calculator.js
+++ simple-lib/calculator.js
@@ -1,5 +1,14 @@
 const sum = (a, b) => {
-    return a + b
+    const n1 = parseInt(a)
+    const n2 = parseInt(b)
+
+    console.log(n1, n2)
+
+    if (isNaN(n1) || isNaN(n2)) {
+        throw new Error('Please check your input')
+    }
+
+    return n1 + n2
 }
 
 module.exports = {
--- simple-lib/test/calculator.spec.js
+++ simple-lib/test/calculator.spec.js
@@ -4,4 +4,17 @@ describe('# Calculator', () => {
     it('-- should sum 2 and 2 and the result must be 4', () => {
         expect(sum(6,2)).toBe(8)
     });
+
+    it('-- should sum 2 and 2 even if one them is a string should receive an error', () => {
+        expect(sum('6','2')).toBe(8)
+    });
+
+    it('-- should throw an error if what is provided to the method cannot number', () => {
+        expect(() => sum('a', 1)).toThrowError()
+        expect(() => sum([2, 1])).toThrowError()
+        expect(() => sum({a, b})).toThrowError()
+        expect(() => sum({})).toThrowError()
+        expect(() => sum()).toThrowError()
+        expect(() => sum('', '')).toThrowError()
+    });
 });
